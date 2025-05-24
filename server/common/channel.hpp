675020497cedd8eccd70935c14972bf3b1a9bfd4
lbk <2162627569@qq.com>
2025-05-24 13:26:50 +0800

    项目基础框架的建立

--- server/common/channel.hpp
+++ server/common/channel.hpp
@@ -6,171 +6,173 @@
 #include <mutex>
 #include "logger.hpp"
 #include <brpc/channel.h>
-
-// 封装单个服务的信道管理类
-class ServiceChannel
+namespace lbk
 {
-public:
-    using ptr = std::shared_ptr<ServiceChannel>;
-    using ChannelPtr = std::shared_ptr<brpc::Channel>;
-    ServiceChannel(const std::string &name)
-        : _service_name(name), _index(0)
+    // 封装单个服务的信道管理类
+    class ServiceChannel
     {
-    }
-    // 服务上线了一个节点，则调用append新增信道
-    void append(const std::string &host)
-    {
-        // 构造Channel信道，连接服务器
-        brpc::ChannelOptions options;
-        options.connect_timeout_ms = -1; // 连接等待超时时间，-1表示一直等待
-        options.timeout_ms = -1;         // rpc请求等待超时时间，-1表示一直等待
-        options.max_retry = 3;           // 请求重试次数
-        options.protocol = "baidu_std";  // 序列化协议，默认使用baidu_std
-        auto channel = std::make_shared<brpc::Channel>();
-        int ret = channel->Init(host.c_str(), &options);
-        if (ret == -1)
+    public:
+        using ptr = std::shared_ptr<ServiceChannel>;
+        using ChannelPtr = std::shared_ptr<brpc::Channel>;
+        ServiceChannel(const std::string &name)
+            : _service_name(name), _index(0)
         {
-            LOG_ERROR("初始化{}-{}信道失败！", _service_name, host);
-            return;
         }
-        std::unique_lock<std::mutex> lock(_mutex);
-        _channels.push_back(channel);
-        _hosts[host] = channel;
-    }
-    // 服务下线了一个节点，则调用remove释放信道
-    void remove(const std::string &host)
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-        auto it = _hosts.find(host);
-        if (it == _hosts.end())
+        // 服务上线了一个节点，则调用append新增信道
+        void append(const std::string &host)
         {
-            LOG_WARN("删除{}-{}节点信道时，没有找到信道信息！", _service_name, host);
-            return;
-        }
-        for (auto vit = _channels.begin(); vit != _channels.end(); vit++)
-        {
-            if (*vit == it->second)
+            // 构造Channel信道，连接服务器
+            brpc::ChannelOptions options;
+            options.connect_timeout_ms = -1; // 连接等待超时时间，-1表示一直等待
+            options.timeout_ms = -1;         // rpc请求等待超时时间，-1表示一直等待
+            options.max_retry = 3;           // 请求重试次数
+            options.protocol = "baidu_std";  // 序列化协议，默认使用baidu_std
+            auto channel = std::make_shared<brpc::Channel>();
+            int ret = channel->Init(host.c_str(), &options);
+            if (ret == -1)
             {
-                _channels.erase(vit);
-                break;
+                LOG_ERROR("初始化{}-{}信道失败！", _service_name, host);
+                return;
             }
+            std::unique_lock<std::mutex> lock(_mutex);
+            _channels.push_back(channel);
+            _hosts[host] = channel;
         }
-        _hosts.erase(it);
-    }
-    // 通过RR轮转策略，获取一个Channel用于发起对应服务的Rpc调用
-    ChannelPtr choose()
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-        if (_channels.size() == 0)
-        {
-            LOG_ERROR("当前没有能够提供{}服务的节点！", _service_name);
-            return ChannelPtr();
-        }
-        int32_t index = _index++ % _channels.size();
-        return _channels[index];
-    }
-
-private:
-    std::mutex _mutex;
-    int32_t _index;                                     // 当前轮转下标的计数器
-    std::string _service_name;                          // 服务名称
-    std::vector<ChannelPtr> _channels;                  // 当前服务对应的信道集合
-    std::unordered_map<std::string, ChannelPtr> _hosts; // 主机地址与信道的映射
-};
-
-// 总体的服务信道管理类
-class ServiceManager
-{
-public:
-    using ptr = std::shared_ptr<ServiceManager>;
-    // 获取指定服务的节点信道
-    ServiceChannel::ChannelPtr choose(const std::string &service_name)
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-        auto it = _services.find(service_name);
-        if (it == _services.end())
-        {
-            LOG_ERROR("当前没有能够提供{}服务的节点！", service_name);
-            return ServiceChannel::ChannelPtr();
-        }
-        return it->second->choose();
-    }
-    // 先声明我关心哪些服务的上下线，不关心的则不需要管理
-    void declared(const std::string &service_name)
-    {
-        std::unique_lock<std::mutex> lock(_mutex);
-        _follow_services.insert(service_name);
-    }
-    // 服务上线时调用的回调接口，将服务节点管理起来
-    void onServiceOnline(const std::string &service_instance, const std::string &host)
-    {
-        std::string service_name = getServiceName(service_instance);
-        ServiceChannel::ptr service;
+        // 服务下线了一个节点，则调用remove释放信道
+        void remove(const std::string &host)
         {
             std::unique_lock<std::mutex> lock(_mutex);
-            auto fit = _follow_services.find(service_name);
-            if (fit == _follow_services.end())
+            auto it = _hosts.find(host);
+            if (it == _hosts.end())
             {
-                LOG_DEBUG("{}-{}服务上线了，但是当前并不关心！", service_name, host);
+                LOG_WARN("删除{}-{}节点信道时，没有找到信道信息！", _service_name, host);
                 return;
             }
-            // 先获取管理对象，没有则创建，有则添加节点
-            auto sit = _services.find(service_name);
-            if (sit == _services.end())
+            for (auto vit = _channels.begin(); vit != _channels.end(); vit++)
             {
-                service = std::make_shared<ServiceChannel>(service_name);
-                _services[service_name] = service;
+                if (*vit == it->second)
+                {
+                    _channels.erase(vit);
+                    break;
+                }
             }
-            else
-                service = sit->second;
+            _hosts.erase(it);
         }
-        if (!service)
-        {
-            LOG_ERROR("新增{}服务管理节点失败！", service_name);
-            return;
-        }
-        service->append(host);
-        LOG_DEBUG("{}-{} 服务上线新节点，进行添加管理！", service_name, host);
-    }
-    // 服务下线时调用的回调接口，从相应的服务节点删除信道
-    void onServiceOffline(const std::string &service_instance, const std::string &host)
-    {
-        std::string service_name = getServiceName(service_instance);
-        ServiceChannel::ptr service;
+        // 通过RR轮转策略，获取一个Channel用于发起对应服务的Rpc调用
+        ChannelPtr choose()
         {
             std::unique_lock<std::mutex> lock(_mutex);
-            auto fit = _follow_services.find(service_name);
-            if (fit == _follow_services.end())
+            if (_channels.size() == 0)
             {
-                LOG_DEBUG("{}-{}服务下线了，但是当前并不关心！", service_name, host);
-                return;
+                LOG_ERROR("当前没有能够提供{}服务的节点！", _service_name);
+                return ChannelPtr();
             }
-            // 先获取管理对象，有则删除节点信道
-            auto sit = _services.find(service_name);
-            if (sit == _services.end())
-            {
-                LOG_WARN("删除{}服务节点时，没有找到管理对象", service_name);
-                return;
-            }
-            else
-                service = sit->second;
+            int32_t index = _index++ % _channels.size();
+            return _channels[index];
         }
-        service->remove(host);
-        LOG_DEBUG("{}-{} 服务下线节点，进行删除管理！", service_name, host);
-    }
 
-private:
-    std::string getServiceName(const std::string &service_instance)
+    private:
+        std::mutex _mutex;
+        int32_t _index;                                     // 当前轮转下标的计数器
+        std::string _service_name;                          // 服务名称
+        std::vector<ChannelPtr> _channels;                  // 当前服务对应的信道集合
+        std::unordered_map<std::string, ChannelPtr> _hosts; // 主机地址与信道的映射
+    };
+
+    // 总体的服务信道管理类
+    class ServiceManager
     {
-        auto pos = service_instance.find_last_of('/');
-        if (pos == std::string::npos)
-            return service_instance;
-        else
-            return service_instance.substr(0, pos);
-    }
+    public:
+        using ptr = std::shared_ptr<ServiceManager>;
+        // 获取指定服务的节点信道
+        ServiceChannel::ChannelPtr choose(const std::string &service_name)
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            auto it = _services.find(service_name);
+            if (it == _services.end())
+            {
+                LOG_ERROR("当前没有能够提供{}服务的节点！", service_name);
+                return ServiceChannel::ChannelPtr();
+            }
+            return it->second->choose();
+        }
+        // 先声明我关心哪些服务的上下线，不关心的则不需要管理
+        void declared(const std::string &service_name)
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            _follow_services.insert(service_name);
+        }
+        // 服务上线时调用的回调接口，将服务节点管理起来
+        void onServiceOnline(const std::string &service_instance, const std::string &host)
+        {
+            std::string service_name = getServiceName(service_instance);
+            ServiceChannel::ptr service;
+            {
+                std::unique_lock<std::mutex> lock(_mutex);
+                auto fit = _follow_services.find(service_name);
+                if (fit == _follow_services.end())
+                {
+                    LOG_DEBUG("{}-{}服务上线了，但是当前并不关心！", service_name, host);
+                    return;
+                }
+                // 先获取管理对象，没有则创建，有则添加节点
+                auto sit = _services.find(service_name);
+                if (sit == _services.end())
+                {
+                    service = std::make_shared<ServiceChannel>(service_name);
+                    _services[service_name] = service;
+                }
+                else
+                    service = sit->second;
+            }
+            if (!service)
+            {
+                LOG_ERROR("新增{}服务管理节点失败！", service_name);
+                return;
+            }
+            service->append(host);
+            LOG_DEBUG("{}-{} 服务上线新节点，进行添加管理！", service_name, host);
+        }
+        // 服务下线时调用的回调接口，从相应的服务节点删除信道
+        void onServiceOffline(const std::string &service_instance, const std::string &host)
+        {
+            std::string service_name = getServiceName(service_instance);
+            ServiceChannel::ptr service;
+            {
+                std::unique_lock<std::mutex> lock(_mutex);
+                auto fit = _follow_services.find(service_name);
+                if (fit == _follow_services.end())
+                {
+                    LOG_DEBUG("{}-{}服务下线了，但是当前并不关心！", service_name, host);
+                    return;
+                }
+                // 先获取管理对象，有则删除节点信道
+                auto sit = _services.find(service_name);
+                if (sit == _services.end())
+                {
+                    LOG_WARN("删除{}服务节点时，没有找到管理对象", service_name);
+                    return;
+                }
+                else
+                    service = sit->second;
+            }
+            service->remove(host);
+            LOG_DEBUG("{}-{} 服务下线节点，进行删除管理！", service_name, host);
+        }
 
-private:
-    std::mutex _mutex;
-    std::unordered_set<std::string> _follow_services;
-    std::unordered_map<std::string, ServiceChannel::ptr> _services;
-};
+    private:
+        std::string getServiceName(const std::string &service_instance)
+        {
+            auto pos = service_instance.find_last_of('/');
+            if (pos == std::string::npos)
+                return service_instance;
+            else
+                return service_instance.substr(0, pos);
+        }
+
+    private:
+        std::mutex _mutex;
+        std::unordered_set<std::string> _follow_services;
+        std::unordered_map<std::string, ServiceChannel::ptr> _services;
+    };
+}
--- server/common/rabbitmq.hpp
+++ server/common/rabbitmq.hpp
@@ -7,101 +7,103 @@
 #include <iostream>
 #include <functional>
 #include "logger.hpp"
-
-class MQClient
+namespace lbk
 {
-public:
-    using ptr = std::shared_ptr<MQClient>;
-    using MessageCallback = std::function<void(const char *, size_t)>;
-    MQClient(const std::string &user, const std::string &password, const std::string &host)
+    class MQClient
     {
-        // 1.实例化底层网络通信框架的IO事件监控句柄
-        _loop = EV_DEFAULT;
-        // 2.实例化LibEvHandler句柄，将AMQP框架与事件监控关联起来
-        _handler = std::make_unique<AMQP::LibEvHandler>(_loop);
-        // 3.实例化连接对象
-        // amqp://root:2162627569@127.0.0.1:5672/
-        std::string url = "amqp://" + user + ":" + password + "@" + host + "/";
-        AMQP::Address address(url);
-        _connection = std::make_unique<AMQP::TcpConnection>(_handler.get(), address);
-        // 4.实例化信道对象
-        _channel = std::make_unique<AMQP::TcpChannel>(_connection.get());
-        // 5.启动底层网络通信框架，开启IO
-        _loop_thread = std::thread([this]()
-                                   { ev_run(_loop, 0); });
-    }
-    ~MQClient()
-    {
-        ev_async_init(&_async_watcher, watcher_callback);
-        ev_async_start(_loop, &_async_watcher);
-        ev_async_send(_loop, &_async_watcher);
-        _loop_thread.join();
-        _loop = nullptr;
-    }
-    void declareComponents(const std::string &exchange, const std::string &queue,
-                           const std::string &routing_key = "routing_key", AMQP::ExchangeType exchange_type = AMQP::ExchangeType::direct)
-    {
-        // 声明交换机
-        _channel->declareExchange(exchange, exchange_type)
-            .onError([&exchange](const char *msg)
-                     {
+    public:
+        using ptr = std::shared_ptr<MQClient>;
+        using MessageCallback = std::function<void(const char *, size_t)>;
+        MQClient(const std::string &user, const std::string &password, const std::string &host)
+        {
+            // 1.实例化底层网络通信框架的IO事件监控句柄
+            _loop = EV_DEFAULT;
+            // 2.实例化LibEvHandler句柄，将AMQP框架与事件监控关联起来
+            _handler = std::make_unique<AMQP::LibEvHandler>(_loop);
+            // 3.实例化连接对象
+            // amqp://root:2162627569@127.0.0.1:5672/
+            std::string url = "amqp://" + user + ":" + password + "@" + host + "/";
+            AMQP::Address address(url);
+            _connection = std::make_unique<AMQP::TcpConnection>(_handler.get(), address);
+            // 4.实例化信道对象
+            _channel = std::make_unique<AMQP::TcpChannel>(_connection.get());
+            // 5.启动底层网络通信框架，开启IO
+            _loop_thread = std::thread([this]()
+                                       { ev_run(_loop, 0); });
+        }
+        ~MQClient()
+        {
+            ev_async_init(&_async_watcher, watcher_callback);
+            ev_async_start(_loop, &_async_watcher);
+            ev_async_send(_loop, &_async_watcher);
+            _loop_thread.join();
+            _loop = nullptr;
+        }
+        void declareComponents(const std::string &exchange, const std::string &queue,
+                               const std::string &routing_key = "routing_key", AMQP::ExchangeType exchange_type = AMQP::ExchangeType::direct)
+        {
+            // 声明交换机
+            _channel->declareExchange(exchange, exchange_type)
+                .onError([&exchange](const char *msg)
+                         {
         LOG_ERROR("{}交换机创建失败：{}",exchange,msg);
         exit(1); })
-            .onSuccess([&exchange]()
-                       { LOG_INFO("{}交换机创建成功！", exchange); });
-        // 声明队列
-        _channel->declareQueue(queue)
-            .onError([&queue](const char *msg)
-                     {
+                .onSuccess([&exchange]()
+                           { LOG_INFO("{}交换机创建成功！", exchange); });
+            // 声明队列
+            _channel->declareQueue(queue)
+                .onError([&queue](const char *msg)
+                         {
         LOG_ERROR("{}队列创建失败：{}",queue,msg);
         exit(1); })
-            .onSuccess([&queue]()
-                       { LOG_INFO("{}队列创建成功！", queue); });
-        // 6.绑定交换机和队列
-        _channel->bindQueue(exchange, queue, routing_key)
-            .onError([&exchange, &queue](const char *msg)
-                     {
+                .onSuccess([&queue]()
+                           { LOG_INFO("{}队列创建成功！", queue); });
+            // 6.绑定交换机和队列
+            _channel->bindQueue(exchange, queue, routing_key)
+                .onError([&exchange, &queue](const char *msg)
+                         {
         LOG_ERROR("{} - {}绑定失败：{}",exchange,queue,msg);
         exit(1); })
-            .onSuccess([&exchange, &queue]()
-                       { LOG_INFO("{} - {}绑定成功！", exchange, queue); });
-    }
-    bool publish(const std::string &exchange, const std::string &msg, const std::string &routing_key = "routing_key")
-    {
-        LOG_DEBUG("向交换机 {}-{} 发布消息！", exchange, routing_key);
-        bool ret = _channel->publish(exchange, routing_key, msg);
-        if (ret == false)
-        {
-            LOG_ERROR("{} 发布消息失败：", exchange);
-            return false;
+                .onSuccess([&exchange, &queue]()
+                           { LOG_INFO("{} - {}绑定成功！", exchange, queue); });
         }
-        return true;
-    }
-   void consume(const std::string &queue, const MessageCallback &cb)
-    {
-        LOG_DEBUG("开始订阅 {} 队列消息！", queue);
-        _channel->consume(queue, "consume-tags")
-            .onReceived([this, &cb](const AMQP::Message &message, uint32_t deliveryTag, bool redelivered)
-                        {
+        bool publish(const std::string &exchange, const std::string &msg, const std::string &routing_key = "routing_key")
+        {
+            LOG_DEBUG("向交换机 {}-{} 发布消息！", exchange, routing_key);
+            bool ret = _channel->publish(exchange, routing_key, msg);
+            if (ret == false)
+            {
+                LOG_ERROR("{} 发布消息失败：", exchange);
+                return false;
+            }
+            return true;
+        }
+        void consume(const std::string &queue, const MessageCallback &cb)
+        {
+            LOG_DEBUG("开始订阅 {} 队列消息！", queue);
+            _channel->consume(queue, "consume-tags")
+                .onReceived([this, &cb](const AMQP::Message &message, uint32_t deliveryTag, bool redelivered)
+                            {
             cb(message.body(),message.bodySize());
             _channel->ack(deliveryTag); })
-            .onError([&queue](const char *message)
-                     {
+                .onError([&queue](const char *message)
+                         {
                     LOG_ERROR("订阅 {} 队列消息失败: {}", queue, message);
                     exit(1); });
-    }
+        }
 
-private:
-    static void watcher_callback(struct ev_loop *loop, ev_async *watcher, int32_t revents)
-    {
-        ev_break(loop, EVBREAK_ALL);
-    }
+    private:
+        static void watcher_callback(struct ev_loop *loop, ev_async *watcher, int32_t revents)
+        {
+            ev_break(loop, EVBREAK_ALL);
+        }
 
-private:
-    struct ev_async _async_watcher;
-    struct ev_loop *_loop;
-    std::unique_ptr<AMQP::LibEvHandler> _handler;
-    std::unique_ptr<AMQP::TcpConnection> _connection;
-    std::unique_ptr<AMQP::TcpChannel> _channel;
-    std::thread _loop_thread;
-};
+    private:
+        struct ev_async _async_watcher;
+        struct ev_loop *_loop;
+        std::unique_ptr<AMQP::LibEvHandler> _handler;
+        std::unique_ptr<AMQP::TcpConnection> _connection;
+        std::unique_ptr<AMQP::TcpChannel> _channel;
+        std::thread _loop_thread;
+    };
+}
